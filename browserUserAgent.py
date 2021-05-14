ffc78f1eefd4cef6dfc523c517b5ce2be18705bc
김현우 <>
2021-05-14 11:37:56 +0900

    no message

--- browserUserAgent.py
+++ browserUserAgent.py
@@ -74,9 +74,9 @@ class BrowserUserAgent:
     def getChromeOptions(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument(self.getBrowerUserAgent())
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         return chrome_options
 
     def getDriver(self, *arglist):
